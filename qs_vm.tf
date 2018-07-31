b42457fab4b6fc3db222acfd2a5771e3250ce90f
clint <clint.carr@outlook.com>
2018-07-31 14:32:23 +1000

    refactor

--- qs_vm.tf
+++ qs_vm.tf
@@ -1,19 +1,3 @@
-data "template_file" "qlik_sense_userdata" {
-  template = "${file("./qlikSenseUserdata.ps1")}"
-
-  vars {
-    user                       = "${var.user}"
-    password                   = "${var.password}"
-    qse_svc_password           = "${var.qse_svc_password}"
-    qse_db_admin_password      = "${var.qse_db_admin_password}"
-    qse_db_repository_password = "${var.qse_db_repository_password}"
-    qse_license                = "${var.qse_license}"
-    qse_control                = "${var.qse_control}"
-    qse_name                   = "${var.qse_name}"
-    qse_org                    = "${var.qse_org}"
-  }
-}
-
 resource "aws_instance" "qlik_sense" {
   ami                    = "${data.aws_ami.qlik_sense_enterprise.id}"
   instance_type          = "${var.ec2_machine_size}"
